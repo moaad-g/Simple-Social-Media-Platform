36dfab93343b67879140f02eb32fe44a81bbe5c3
moaad-g <mgountetti27@outlook.com>
2023-12-14 00:14:56 +0000

    added phone number to profile page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,9 +3,13 @@
 @section('title', $user->information->name )
 
 @section('content')
-<h1>{{ $user->email }}</h1>
+
+<div class="flex items-center justify-center space-x-5">
+    <p class="font-bold text-m">Email: {{ $user->email }}</p>
+    <p class="font-bold text-m">Number: {{ $user->information->phone_number }}</p>
+</div>
 <h1 class="font-bold">Posts:</h1>
-<div class="grid grid-cols-3 gap-3">
+<div class="grid grid-cols-3 gap-3 py-2">
     @foreach ($post_list as $post)
         <div class="bg-gray-600 rounded text-white py-1 px-1 text-sm hover:bg-gray-400 border-solid border-2 borer-color border-gray-700 ">
             <button><a href="{{ route('posts.show', ['id'=>$post->id]) }}"> {{ $post->content }}</a></button>
